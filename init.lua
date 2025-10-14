c3ce7f1bff379584ef77316583ad58f45077beee
LeonBethuyneSomko <leon.bethuyne@somko.be>
2025-10-14 14:12:59 +0200

    updates

--- init.lua
+++ init.lua
@@ -893,28 +893,6 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'maxm03/fluoromachine.nvim',
-    lazy = false,
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      local fm = require 'fluoromachine'
-
-      fm.setup {
-        glow = true,
-        theme = 'fluoromachine',
-        transparent = true,
-      } -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'fluoromachine'
-    end,
-  },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
